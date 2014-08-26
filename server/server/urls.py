bc68bd1781b375ab844129b655f554e30e55923d
NullMode <nullmode@gmail.com>
2014-08-26 22:15:35 +0100

    Adds names to URLs for reverse lookups. Loads static css file "better" Uses reverse URL look ups in template

--- server/server/urls.py
+++ server/server/urls.py
@@ -4,10 +4,10 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', 'server.views.home'),
-    url(r'^node/([0-9]{0,5})/$', 'server.views.node'),  # supports up to 10000 nodes
-    url(r'^register/$', 'server.views.register'),
-    url(r'^status/$', 'server.views.status'),
-    url(r'^crash/$', 'server.views.crash'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'server.views.home', name='home'),
+    url(r'^node/([0-9]{0,5})/$', 'server.views.node', name='node'),  # supports up to 10000 nodes
+    url(r'^register/$', 'server.views.register', name='register'),
+    url(r'^status/$', 'server.views.status', name='status'),
+    url(r'^crash/$', 'server.views.crash', name='crash'),
+    url(r'^admin/', include(admin.site.urls), name='admin'),
 )
